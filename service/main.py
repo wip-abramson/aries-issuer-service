28e2321453c1ed7bdd1acd8219ae46566879d05a
wip-abramson <wip.abramson@gmail.com>
2021-05-31 16:39:22 +0100

    update to latest acc, make issue hlgf creds

--- service/main.py
+++ service/main.py
@@ -5,7 +5,11 @@ import json
 import sys
 import time
 import asyncio
-from aries_basic_controller import AriesAgentController
+from aries_cloudcontroller import AriesAgentController
+from datetime import date
+import nest_asyncio
+
+nest_asyncio.apply()
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
@@ -18,29 +22,67 @@ root.addHandler(handler)
 
 
 
-WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
-WEBHOOK_PORT = os.getenv('WEBHOOK_PORT')
-WEBHOOK_BASE = os.getenv('WEBHOOK_BASE')
-ADMIN_URL = os.getenv('ADMIN_URL')
 
-root.info(f"Initialising Agent. Webhook URL {WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_BASE}. ADMIN_URL - {ADMIN_URL}")
-
-
-schema_id = "D4zNtr48UJy7MdbsupTKbF:2:SSI Duet Tutorial:0.0.1"
+schema_id = 'W4t2Pa4XR1qBDvwhVcn4CY:2:Aries ACA ACC Jupyter Playground Demo Participation:0.0.1'
 cred_def_id = None
 
-agent_controller = AriesAgentController(webhook_host=WEBHOOK_HOST, webhook_port=WEBHOOK_PORT,
-                                       webhook_base=WEBHOOK_BASE, admin_url=ADMIN_URL)
+api_key = os.getenv("ACAPY_ADMIN_API_KEY")
+admin_url = os.getenv("ADMIN_URL")
 
+agent_controller = AriesAgentController(admin_url,api_key)
 
 def cred_handler(payload):
-    print("Handle Credentials")
+    connection_id = payload['connection_id']
     exchange_id = payload['credential_exchange_id']
     state = payload['state']
     role = payload['role']
-    attributes = payload['credential_proposal_dict']['credential_proposal']['attributes']
-    print(f"Credential exchange {exchange_id}, role: {role}, state: {state}")
-    print(f"Offering: {attributes}")
+    print("\n---------------------------------------------------\n")
+    print("Handle Issue Credential Webhook")
+    print(f"Connection ID : {connection_id}")
+    print(f"Credential exchange ID : {exchange_id}")
+    print("Agent Protocol Role : ", role)
+    print("Protocol State : ", state)
+    print("\n---------------------------------------------------\n")
+    loop = asyncio.get_event_loop()
+    if state == "proposal_received":
+        print(f'Proposal Comment : {payload["credential_proposal_dict"]["comment"]}')
+        proposed_cred_def_id = payload["credential_proposal_dict"]["cred_def_id"]
+        proposed_schema_id = payload["credential_proposal_dict"]["schema_id"]
+
+        if proposed_cred_def_id == cred_def_id and proposed_schema_id == schema_id:
+            participant = None
+            description = None
+            for attribute in payload["credential_proposal_dict"]["credential_proposal"]["attributes"]:
+                if attribute["name"] == "Participant":
+                    participant = attribute["value"]
+                elif attribute["name"] == "Description":
+                    description = attribute["value"]
+                    description += "\n Thanks for attending!"
+
+            event_name = "Hyperledger Global Forum 2021"
+            issue_date = date.today().isoformat()
+            credential_attributes = [
+                {"name": "Event Name", "value": event_name},
+                {"name": "Participant", "value": participant},
+                {"name": "Description", "value": description},
+                {"name": "Date", "value": issue_date}
+            ]
+
+            print(credential_attributes)
+
+            # Do you want the ACA-Py instance to trace it's processes (for testing/timing analysis)
+            trace = False
+            comment = ""
+            # Remove credential record after issued?
+            auto_remove = True
+
+            # Change <schema_id> and <cred_def_id> to correct pair. Cred_def_id must identify a definition to which your agent has corresponding private issuing key.
+            loop.run_until_complete(
+                agent_controller.issuer.send_credential(connection_id, schema_id, cred_def_id, credential_attributes,
+                                                        comment, auto_remove, trace))
+        else:
+            explanation = "You have not referenced the correct schema and credential definitions"
+            loop.run_until_complete(agent_controller.issuer.send_problem_report(exchange_id, explanation))
 
 
 cred_listener = {
@@ -50,21 +92,24 @@ cred_listener = {
 
 
 def connections_handler(payload):
-    global STATE
+    state = payload['state']
     connection_id = payload["connection_id"]
-    print("Connection message", payload, connection_id)
-    STATE = payload['state']
-    if STATE == 'active':
-        #         print('Connection {0} changed state to active'.format(connection_id))
-        root.info("Connection {0} changed state to active".format(connection_id))
+    their_role = payload["their_role"]
+    routing_state = payload["routing_state"]
 
-        credential_attributes = [
-            {"name": "connection_permitted", "value": "1"}
-        ]
-        loop = asyncio.get_event_loop()
-        loop.create_task(
-            agent_controller.issuer.send_credential(connection_id, schema_id, cred_def_id, credential_attributes,
-                                                    trace=False))
+    print("----------------------------------------------------------")
+    print("Connection Webhook Event Received")
+    print("Connection ID : ", connection_id)
+    print("State : ", state)
+    print("Routing State : ", routing_state)
+    print("Their Role : ", their_role)
+    print("----------------------------------------------------------")
+
+    print(payload)
+
+    if state == "active":
+        # Your business logic
+        print("Connection ID: {0} is now active.".format(connection_id))
 
 
 connection_listener = {
@@ -75,7 +120,12 @@ connection_listener = {
 
 async def initialise():
     print("INITIALISING THE AGENT")
-    await agent_controller.listen_webhooks()
+    webhook_port = int(os.getenv("WEBHOOK_PORT"))
+    webhook_host = "0.0.0.0"
+
+    await agent_controller.init_webhook_server(webhook_host, webhook_port)
+
+    print(f"Listening for webhooks from agent at http://{webhook_host}:{webhook_port}")
 
     agent_controller.register_listeners([cred_listener, connection_listener], defaults=True)
 
@@ -88,25 +138,25 @@ async def initialise():
         except:
             time.sleep(5)
 
-    # response = await agent_controller.wallet.get_public_did()
-    #
-    # print("PUBLIC DID", response)
-    # if not response['result']:
-    did = await write_public_did()
-    print(f"Public DID {did} written to the ledger")
+    response = await agent_controller.wallet.get_public_did()
+
+    print("PUBLIC DID", response)
+    if not response['result']:
+        did = await write_public_did()
+        print(f"Public DID {did} written to the ledger")
 
 
-    # Write Cred Def and Schema to ledger
-    response = await agent_controller.definitions.write_cred_def(schema_id)
+        # Write Cred Def and Schema to ledger
+        response = await agent_controller.definitions.write_cred_def(schema_id)
 
-    global cred_def_id
-    cred_def_id = response["credential_definition_id"]
-    logging.info(f"Credential Definition {cred_def_id} for schema {schema_id}")
+        global cred_def_id
+        cred_def_id = response["credential_definition_id"]
+        logging.info(f"Credential Definition {cred_def_id} for schema {schema_id}")
 
-    # Create Invitation
-    invite = await agent_controller.connections.create_invitation(multi_use="true")
-    logging.info("Multi Use Connection Invitation")
-    logging.info(invite["invitation"])
+        # Create Invitation
+        invite = await agent_controller.connections.create_invitation(multi_use="true")
+        logging.info("Multi Use Connection Invitation")
+        logging.info(invite["invitation"])
 
 
 async def write_public_did():
